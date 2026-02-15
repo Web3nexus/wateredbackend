0a3a0252fdd219b77c6c5d16c418e8a20b656431
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-02-15 08:10:11 -0800

    debug

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,27 @@ use App\Http\Controllers\Api\V1\EventController;
 
 Route::get('/', [LandingPageController::class, 'index'])->name('home');
 
+Route::get('/debug-config', function () {
+    $storagePath = storage_path('app/public');
+    $publicLink = public_path('storage');
+
+    return [
+        'app_url' => config('app.url'),
+        'asset_url' => config('app.asset_url'),
+        'filesystem_driver' => config('filesystems.default'),
+        'public_disk_config' => config('filesystems.disks.public'),
+        'storage_path' => $storagePath,
+        'storage_path_exists' => is_dir($storagePath),
+        'storage_path_perms' => substr(sprintf('%o', fileperms($storagePath)), -4),
+        'public_link' => $publicLink,
+        'public_link_exists' => file_exists($publicLink),
+        'public_link_is_link' => is_link($publicLink),
+        'public_link_target' => is_link($publicLink) ? readlink($publicLink) : 'NOT_A_LINK',
+        'files_in_storage' => \Illuminate\Support\Facades\File::files($storagePath),
+        'directories_in_storage' => \Illuminate\Support\Facades\File::directories($storagePath),
+    ];
+});
+
 Route::get('/events', [EventsListingController::class, 'index'])->name('events.index');
 Route::get('/events/{event}', [EventsListingController::class, 'show'])->name('events.show');
 Route::post('/events/{event}/register', [EventsListingController::class, 'register'])->name('events.register');
