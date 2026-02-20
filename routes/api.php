4d8b760146e978e86c8eb32537babefad11f3bb4
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-02-20 00:47:26 -0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -82,11 +82,12 @@ Route::prefix('v1')->group(function () {
         Route::middleware('premium')->group(function () {
             Route::get('/rituals', [\App\Http\Controllers\Api\V1\RitualController::class, 'index']);
             Route::get('/rituals/{ritual}', [\App\Http\Controllers\Api\V1\RitualController::class, 'show']);
-            Route::get('/deities', [\App\Http\Controllers\Api\V1\DeityController::class, 'index']);
-            Route::get('/deities/{deity}', [\App\Http\Controllers\Api\V1\DeityController::class, 'show']);
             Route::get('/incantations', [\App\Http\Controllers\Api\V1\IncantationController::class, 'index']);
             Route::get('/incantations/{incantation}', [\App\Http\Controllers\Api\V1\IncantationController::class, 'show']);
         });
+
+        Route::get('/deities', [\App\Http\Controllers\Api\V1\DeityController::class, 'index']);
+        Route::get('/deities/{deity}', [\App\Http\Controllers\Api\V1\DeityController::class, 'show']);
 
         Route::get('/holidays', [\App\Http\Controllers\Api\V1\HolidayController::class, 'index']);
 
